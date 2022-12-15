f5034d19dee0422defde6c76c12c05d7d57ccd65
practicantestigo <tigounepracticantes@gmail.com>
2022-12-15 15:33:51 -0500

    enviando correos

--- index.js
+++ index.js
@@ -2,12 +2,36 @@ const express = require('express');
 const app = express();
 let cors = require('cors');
 const bodyParser = require('body-parser');
+const nodeMailer = require('nodemailer');
 
 app.use(cors());
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use(require('./routes/correoRoutes'));
+app.post('/envio', async (req, res) => {
+    let transporter = nodeMailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth:{
+            user: 'kawadsign@gmail.com',
+            pass: 'ogftlaunzksdayew'
+        }
+    })
+
+    let info = await transporter.sendMail({
+        from: 'kawadsign@gmail.com',
+        subject: req.body.service,
+        to: 'kawadsign@gmail.com',
+        text: JSON.stringify(req.body)
+    })
+
+    if (info) {
+        res.send(JSON.parse('{"response": "Tu cita esta siendo evaluada"}'));
+    }else{
+        res.send(JSON.parse('{"error": "problemas al enviar tu peticion"}'));
+    }
+});
 
 app.listen('3000', () => {
     console.log('escuchando');
